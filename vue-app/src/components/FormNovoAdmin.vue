60cdbbf02dfc5292966d1bcd11d12f7e07af1a4e
Edson Yamamoto <edsonkazyamamoto@gmail.com>
2019-03-06 23:42:55 -0300

    Adicionado valor do input

---
+++ vue-app/src/components/FormNovoAdmin.vue
@@ -0,0 +1,58 @@
+<template>
+    <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation 
+    @submit.prevent
+  >
+    <v-text-field
+      v-model="nome"
+      :counter="10"
+      :rules="nameRules"
+      label="Name"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+
+    <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      :disabled="!valid"
+      color="success"
+      @click="AddUserForm"
+    >
+      Validate
+    </v-btn>
+
+  </v-form>
+</template>
+<style>
+</style>
+<script>
+export default {
+  data() {
+    return {
+      nome: "",
+      email: "",
+      modelo: ""
+    };
+  },
+  methods: {
+    AddUserForm: function() {
+      console.log(this.nome);
+      console.log(this.email);
+    }
+  }
+};
+</script>
--- vue-app/src/view/Dashboard.vue
+++ vue-app/src/view/Dashboard.vue
@@ -3,28 +3,28 @@
         <v-navigation-drawer v-model="drawer" fixed clipped class="grey lighten-4" app>
         <v-list dense class="grey lighten-4">
             <template v-for="(item, i) in items">
-            <v-layout v-if="item.heading" :key="i" row align-center >
-                <v-flex xs6>
-                <v-subheader v-if="item.heading">
-                    {{ item.heading }}
-                </v-subheader>
-                </v-flex>
-                <v-flex xs6 class="text-xs-right">
-                <v-btn small flat>edit</v-btn>
-                </v-flex>
-            </v-layout>
-            <v-divider v-else-if="item.divider" :key="i" dark class="my-3" ></v-divider>
+              <v-layout v-if="item.heading" :key="i" row align-center >
+                  <v-flex xs6>
+                  <v-subheader v-if="item.heading">
+                      {{ item.heading }}
+                  </v-subheader>
+                  </v-flex>
+                  <v-flex xs6 class="text-xs-right">
+                  <v-btn small flat>edit</v-btn>
+                  </v-flex>
+              </v-layout>
+              <v-divider v-else-if="item.divider" :key="i" dark class="my-3" ></v-divider>
 
-            <v-list-tile v-else :key="i" @click="AtualizarTela(item.function)">
-                <v-list-tile-action>
-                    <v-icon>{{ item.icon }}</v-icon>
-                </v-list-tile-action>
-                <v-list-tile-content>
-                    <v-list-tile-title class="grey--text">
-                        {{ item.text }}
-                    </v-list-tile-title>
-                </v-list-tile-content>
-            </v-list-tile>
+              <v-list-tile v-else :key="i" @click="AtualizarTela(item.function)">
+                  <v-list-tile-action>
+                      <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-tile-action>
+                  <v-list-tile-content>
+                      <v-list-tile-title class="grey--text">
+                          {{ item.text }}
+                      </v-list-tile-title>
+                  </v-list-tile-content>
+              </v-list-tile>
             </template>
         </v-list>
         </v-navigation-drawer>
@@ -47,6 +47,9 @@
                 <main-dashboard v-if="this.tela==''"></main-dashboard>
                 <user-form v-if="this.tela=='notas'"></user-form>
                 <professor-form v-if="this.tela=='acessso'"></professor-form>
+                <form-novo-admin v-if="this.tela=='novoAdmin'"></form-novo-admin>
+                <input-file v-if="this.tela=='inputFile'"></input-file>
+                
             </v-flex>
             </v-layout>
         </v-container>
@@ -60,12 +63,16 @@
 import userForm from "@/components/UserForm";
 import professorForm from "@/components/ProfessorForm";
 import mainDashboard from "@/components/MainDashboard";
+import formNovoAdmin from "@/components/FormNovoAdmin";
+import inputFile from "@/components/InputFile";
 
 export default {
   components: {
     mainDashboard,
     userForm,
-    professorForm
+    professorForm,
+    formNovoAdmin,
+    inputFile
   },
   data: () => ({
     drawer: null,
@@ -84,6 +91,16 @@ export default {
         icon: "fas fa-fingerprint",
         text: "Reminders",
         function: "acessso"
+      },
+      {
+        icon: "fas fa-plus",
+        text: "Novo administrador",
+        function: "novoAdmin"
+      },
+      {
+        icon: "fas fa-plus",
+        text: "Adicionar csv",
+        function: "inputFile"
       },
       { divider: true },
       { heading: "Labels" },
